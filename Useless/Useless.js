5eb165ed3fdd91a66e6c724b00fd594cb3d7e24b
PhilipPaulson <pnp4@illinois.edu>
2022-10-01 19:17:11 -0500

    Since the React Torrent isn't up yet, I decided to add more to Useless Button.

--- Useless/Useless.js
+++ Useless/Useless.js
@@ -1,16 +1,41 @@
 const useless = document.querySelector(".btn");
-const header = document.querySelector("h1");
-const subtitle = document.querySelector("h2");
+const header = document.querySelector("#title");
+const subtitle = document.querySelector("#subtitle");
+
 const wow = new Audio("wow.mp3");
+const thanks = new Audio("thanks.mp3");
+
+let clicked = false;
+let first = true;
 
 useless.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log("wow");
+  console.log("click");
 
-  // Got this from stack overflow
-  if (wow.paused) {
-    wow.play();
-  } else {
-    wow.currentTime = 0;
+  if (first) {
+    clicked = true;
+
+    // Got this from stack overflow
+    if (wow.paused) {
+      wow.play();
+    } else {
+      wow.currentTime = 0;
+    }
+  } else if (clicked) {
+    // console.log("thanks");
+    clicked = false;
+    header.innerHTML = "Thanks for messing with the Useless button!";
+    subtitle.innerHTML = "There's nothing else to do here now.";
+    useless.value = "Truly Useless";
+    thanks.play();
+  }
+});
+
+useless.addEventListener("mouseout", (e) => {
+  if (clicked && first) {
+    first = false;
+    // console.log("moused out");
+    header.innerHTML = "Wait, no, come back!";
+    subtitle.innerHTML = "Please press the button just one more time!";
   }
 });
