17f729075c4da9aa845e929777dffc05d502abdc
PhilipPaulson <pnp4@illinois.edu>
2022-10-06 18:20:34 -0500

    Created Feedback Item, started implementing it

--- React Tutorial/feedback-app/src/App.js
+++ React Tutorial/feedback-app/src/App.js
@@ -1,10 +1,12 @@
 import Header from "./components/Header";
+import FeedbackItem from "./components/FeedbackItem";
 
 function App() {
   return (
     <>
       <Header />
       <div className="container">
+        <FeedbackItem />
         <h1>My App</h1>
       </div>
     </>
