6af6579fced6fee2780c42ceb0215f57ddb51232
PhilipPaulson <pnp4@illinois.edu>
2022-10-19 18:04:23 -0500

    set up Router for react-dom 5

--- React Tutorial/feedback-app/src/App.js
+++ React Tutorial/feedback-app/src/App.js
@@ -1,10 +1,12 @@
 import { v4 as uuidv4 } from "uuid";
+import { BroswerRouter as Router, Route } from "react-router-dom";
 import { useState } from "react";
 import Header from "./components/Header";
 import FeedbackList from "./components/FeedbackList";
 import FeedbackData from "./data/FeedbackData";
 import FeedbackStats from "./components/FeedbackStats";
 import FeedbackForm from "./components/FeedbackForm";
+import AboutPage from "./pages/AboutPage";
 
 function App() {
   const [feedback, setFeedback] = useState(FeedbackData);
@@ -21,15 +23,18 @@ function App() {
   };
 
   return (
-    <>
+    <Router>
       <Header />
       <div className="container">
-        <FeedbackForm handleAdd={addFeedback} />
-        <FeedbackStats feedback={feedback} />
-        <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
-        <h1>My App</h1>
+        <Route exact path="/">
+          <FeedbackForm handleAdd={addFeedback} />
+          <FeedbackStats feedback={feedback} />
+          <FeedbackList feedback={feedback} handleDelete={deleteFeedback} />
+        </Route>
+
+        <Route path="/about" component={AboutPage} />
       </div>
-    </>
+    </Router>
   );
 }
 
